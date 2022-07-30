e14763c0c3b035222f6fd7e79e3d73e64448b309
SebastianVillaL <Sebastian.VillaL@outlook.com>
2022-07-29 21:04:49 -0500

    Added the schema for the posts created by users to predefine the data we expect, also corrected the createpost name to follow best practices

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body #Allows to extract data from the Body of a POST Request
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel): #This is a schema(to format our posts with the following criteria"
+	title: str
+	content: str
+	published: bool = True
+	rating: Optional[int] = None
+	
 
 @app.get("/")
 async def root():
@@ -11,8 +20,7 @@ async def root():
 def get_posts():
 	return {"data": "This are your posts"}
 
-@app.post("/createposts")
-#This receives the Body of a POST request, converts to python dict, then stores on variable payLoad
-def create_posts(payLoad: dict = Body(...)):
-	print(payLoad)
-	return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+@app.post("/posts")
+#This receives the Body of a POST request, then stores on variable payLoad
+def create_posts(payLoad: Post):
+	return {"data": "new post"}
